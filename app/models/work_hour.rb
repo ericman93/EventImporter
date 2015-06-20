bad4d48314210c0a2a38e7f15e73dae6b2e455eb
Eric <ericfeldman93@gmail.com>
2015-06-20 23:20:51 +0300

    change default work hours

--- app/models/work_hour.rb
+++ app/models/work_hour.rb
@@ -12,14 +12,20 @@ class WorkHour < ActiveRecord::Base
 	def self.build_work_day(day_name)
 		day = WorkHour.new
 		day.day = day_name
-		day.start_at = Tod::TimeOfDay.new 0
+		day.start_at = Tod::TimeOfDay.new 9
 		# the default workhours is full day
-		day.end_at = Tod::TimeOfDay.new 0
+		day.end_at = Tod::TimeOfDay.new 18
 		day
 	end
 
 	def self.build_week
-		Constant::DAYS.map{|day_name| self.build_work_day(day_name)}
+		days = Constant::DAYS.map{|day_name| self.build_work_day(day_name)}
+		days[-1].start_at = 0;
+		days[-1].end_at = 0;
+		days[-2].start_at = 0;
+		days[-2].end_at = 0;
+
+		return days
 	end
 
 	def short_day_name
