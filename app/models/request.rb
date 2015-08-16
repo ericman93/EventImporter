2e92f1cc409358d8d047364170e4e8e9d10f3926
Eric <ericfeldman93@gmail.com>
2015-08-16 13:59:50 +0300

    move validations to bottom

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,9 @@
 class Request < ActiveRecord::Base
-	validates_format_of :return_mail, :with => /.+@.+/ ,:message => "Invalid email address" 
-	validates :return_name, :presence => { :message => "Return name is required" }
-	validates :subject, :presence => { :message => "Subject is required" }
-
 	belongs_to :user
 	has_many :request_proposals, :dependent => :destroy 
 
+	validates_format_of :return_mail, :with => /.+@.+/ ,:message => "Invalid email address" 
+	validates :return_name, :presence => { :message => "Return name is required" }
+	validates :subject, :presence => { :message => "Subject is required" }
 	#subject , location , return_mail , return_name
 end
